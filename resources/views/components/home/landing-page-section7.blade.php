0f561b7d9b9a32d5eb7e4a8db646d2e0b85ba407
ProgrammerJaneki <revillazjannelz@gmail.com>
2022-07-21 00:32:10 +0800

    Almost Finished in section7

--- resources/views/components/home/landing-page-section7.blade.php
+++ resources/views/components/home/landing-page-section7.blade.php
@@ -1,5 +1,95 @@
-<div class="bg-white w-full">
-    <section class="px-6 md:px-[100px]">
-        Section 7
+<div class="bg-white w-full grid place-items-center">
+    <section class="px-6 md:px-[100px]  bg-white">
+        <!-- Container -->
+        <div class="md:flex flex-col lg:flex-row gap-12 grid-cols-2">
+            <!-- Col 1 -->
+            <div class="max-w-[582px] w-full ">
+                <!-- Upper -->
+                <div class="w-full flex flex-col">
+                    <span class="font-bold text-sm md:text-lg text-[#FFB23F]">Articles</span>
+                    <span class="font-bold text-2xl md:text-[2.75rem] mt-2 md:mt-[0.88rem] leading-[1.3] text-[#151411]">The best furniture comes <br> from Lalasia</span>
+                    <span class="font-black text-sm md:text-lg w-[327px] md:w-[544px] text-[#AFADB5] mt-4 md:mt-[1.88rem] leading-[1.8]">Pellentesque etiam blandit  in tincidunt at donec. </span>
+                </div>
+                <!-- Col 1 Image -->
+                <!-- Container for control -->
+                <div class="mt-1 md:mt-[50px] items-center">
+                    <img class="absolute max-w-[327px] md:max-w-[550px] w-full lg:max-w-[582px] " src="{{ asset('images/landingImages/section7-cozy.png') }}" alt="img">
+                    <!-- Text Inside Image -->
+                    <div class="text-truncate relative z-50 left-0 top-[12rem] md:top-[24rem] whitespace-nowrap lg:top-[24rem] xl:top-[24rem] ml-8  flex flex-col text-white max-w-[327px] md:max-w-[530px] ">
+                        <span class="font-bold md:font-black text-xs md:text-[0.9rem] lg:text-[1rem] xl:text-[1.1rem] xl:text-lg leading-[1.8]">Tips and Trick</span>
+                        <span class="font-bold text-base md:text-[1.3rem] lg:text-[1.5rem] xl:text-[1.63rem] lg:leading[1.3] xl:leading-[1.3] mt-[0.38rem] md:mt-[0.63rem]">Create Cozy Dinning Room Vibes</span>
+                        <span class="overflow-hidden text-ellipsis font-black text-sm md:text-[1rem] lg:text-[1.1rem] xl:text-lg lg:leading-[1.8rem] xl:leading-[1.8] mt-[0.38rem]">Decorating with neutrals brings balance <span class="hidden md:inline-block">to the dining roo...</span> </span>
+                        <span class="font-bold text-xs md:text-[1rem] lg:text-[1.1rem] xl:text-lg mt-3 md:mt-[0.88rem]">Read More</span>
+                    </div>
+                    <div class="absolute hidden md:flex ml-[25.6rem] lg:ml-[27.7rem] mt-[23.7rem] lg:mt-[25rem] xl:mt-[23.5rem]">
+                        <div class="bg-white text-black p-[21px] max-w-[70px] max-h-[70px]">
+                            <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img"  preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6L2 16l8 10M2 16h28"/>
+                            </svg>
+                        </div>
+                        <div class="bg-[#518581] text-white p-[21px] max-w-[70px] max-h-[70px]">
+                            <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m22 6l8 10l-8 10m8-10H2"/>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Col 2 -->
+            <div class="flex flex-col justify-center gap-5 md:gap-8 mt-[250px] md:mt-[430px] lg:mt-0">
+                <!-- Item 1 -->
+                <div class="flex flex-row md:flex-col xl:flex-row items-start justify-center md:items-center gap-6 w-full">
+                    <!-- Image -->
+                    <img class="w-full max-w-[110px] md:max-w-[260px] lg:max-w-[290px] lg:max-w-auto" src="{{ asset('images/landingImages/section7-top.png') }}" alt="img">
+                    <!-- Right Side Info -->
+                    <div class="flex justify-center flex-col w-full max-w-[329px] md:max-w-[329px]">
+                        <span class="font-black text-xs md:text-lg text-[#AFADB5] leading-[1.8]">Tips and Trick</span>
+                        <span class="font-bold text-base md:text-2xl text-[#151411] mt-1 md:mt-6 leading-[1.3]">6 ways to give your home minimalistic vibes</span>
+                        <span class="hidden md:inline-block font-black mt-[0.88rem] text-base text-[#AFADB5] leading-[1.8]">Pellentesque etiam blandit in..</span>
+                        <div class="flex justify-between items-center mt-[17px]">
+                            <div class="flex items-center">
+                                <img class="w-[28px] max-h-[28px]" src="{{ asset('images/landingImages/section7-jean.png') }}" alt="img">
+                                <span class="font-bold text-xs md:text-sm ml-[10px] text-[#151411] leading-[1.8] md:leading-[1.3]">By Jeremy Jean</span>
+                            </div>
+                            <span class="hidden md:inline-block font-black text-sm mr-[29px] text-[#AFADB5]">Friday, 1 April 2022</span>
+                        </div>
+                    </div>
+                </div>
+                <!-- Item 2 -->
+                <div class="flex flex-row md:items-center xl:flex-row md:flex-col items-start gap-6 w-full">
+                    <!-- Image -->
+                    <img class="w-full max-w-[110px] md:max-w-[260px] lg:max-w-[290px] max-h-[260px]" src="{{ asset('images/landingImages/section7-middle.png') }}" alt="img">
+                    <!-- Right Side Info -->
+                    <div class="flex justify-center flex-col w-full max-w-[329px] md:max-w-[329px]">
+                        <span class="font-black text-xs md:text-lg text-[#AFADB5] leading-[1.8]">Design Inspiration</span>
+                        <span class="font-bold text-base md:text-2xl text-[#151411] mt-1 md:mt-6 leading-[1.3]">How to make your interiors cooler and more stylish</span>
+                        <span class="hidden md:inline-block font-black mt-[0.88rem] text-base text-[#AFADB5] leading-[1.8]">Pellentesque etiam blandit in..</span>
+                        <div class="flex justify-between items-center mt-[17px]">
+                            <div class="flex items-center">
+                                <img class="w-[28px] max-h-[28px]" src="{{ asset('images/landingImages/section7-augus.png') }}" alt="img">
+                                <span class="font-bold text-xs md:text-sm ml-[10px] text-[#151411] leading-[1.8] md:leading-[1.3]">By Jeremy Jean</span>
+                            </div>
+                            <span class="hidden md:inline-block font-black text-sm mr-[29px] text-[#AFADB5]">Friday, 1 April 2022</span>
+                        </div>
+                    </div>
+                </div>
+                <!-- Item 3 -->
+                <div class="flex flex-row md:flex-col xl:flex-row items-start md:items-center gap-6 w-full">
+                    <!-- Image -->
+                    <img class="w-full max-w-[110px] md:max-w-[260px] lg:max-w-[290px]" src="{{ asset('images/landingImages/section7-bottom.png') }}" alt="img">
+                    <!-- Right Side Info -->
+                    <div class="flex justify-center flex-col w-full max-w-[329px] md:max-w-[329px]">
+                        <span class="font-black text-xs md:text-lg text-[#AFADB5] leading-[1.8]">Tips and Trick</span>
+                        <span class="font-bold text-base md:text-2xl text-[#151411] mt-1 md:mt-6 leading-[1.3]">Elements to add character to your space</span>
+                        <span class="hidden md:inline-block font-black mt-[0.88rem] text-base text-[#AFADB5] leading-[1.8]">Pellentesque etiam blandit in..</span>
+                        <div class="flex justify-between items-center mt-[17px]">
+                            <div class="flex items-center">
+                                <img class="w-[28px] max-h-[28px]" src="{{ asset('images/landingImages/section7-kim.png') }}" alt="img">
+                                <span class="font-bold text-xs md:text-sm ml-[10px] text-[#151411] leading-[1.8] md:leading-[1.3]">By Jeremy Jean</span>
+                            </div>
+                            <span class="hidden md:inline-block font-black text-sm mr-[29px] text-[#AFADB5]">Friday, 1 April 2022</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </div>
