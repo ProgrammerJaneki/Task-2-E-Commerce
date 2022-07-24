a057b744b85d1a5ea8b57162830236318dbe5827
ProgrammerJaneki <revillazjannelz@gmail.com>
2022-07-24 18:14:29 +0800

    Added Layouts

---
+++ resources/views/components/layouts/base.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>E-Commerce</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet">
+    <!-- Tailwindcss -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Alpine -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+</head>
+
+<body class="">
+    <!-- Navigation Bar -->
+    {{--
+    <x-home.landing-page /> --}}
+
+    @yield('content')
+    <!-- Footer -->
+    <x-home.landing-page-footer />
+</body>
+
+</html>
